e025aac2fcc9e41f724582400abeca1db6a508c2
kevinlindoames <kevinlindo@hotmail.com>
2026-06-11 03:46:52 -0500

    chore: prepare netlify deployment

--- apps/shell/vite.config.ts
+++ apps/shell/vite.config.ts
@@ -1,32 +1,38 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
 import federation from '@originjs/vite-plugin-federation';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    federation({
-      name: 'shell',
-      remotes: {
-        pokemonDetailMf: 'http://localhost:3001/assets/remoteEntry.js',
-        pokemonHistoryMf: 'http://localhost:3002/assets/remoteEntry.js',
-      },
-      shared: ['react', 'react-dom'],
-    }),
-  ],
-  server: {
-    port: 3000,
-    strictPort: true,
-  },
-  preview: {
-    port: 3000,
-    strictPort: true,
-  },
-  build: {
-    target: 'esnext',
-    minify: false,
-    cssCodeSplit: false,
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+      federation({
+        name: 'shell',
+        remotes: {
+          pokemonDetailMf:
+            env.VITE_DETAIL_REMOTE_URL ??
+            'http://localhost:3001/assets/remoteEntry.js',
+          pokemonHistoryMf:
+            env.VITE_HISTORY_REMOTE_URL ??
+            'http://localhost:3002/assets/remoteEntry.js',
+        },
+        shared: ['react', 'react-dom'],
+      }),
+    ],
+    server: {
+      port: 3000,
+      strictPort: true,
+    },
+    preview: {
+      port: 3000,
+      strictPort: true,
+    },
+    build: {
+      target: 'esnext',
+    },
+  };
 });
